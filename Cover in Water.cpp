5d1668e94b47294025082bc4d2b67ad05200f5c6
Isha Upadhyay <148354274+Isha-upadhyay@users.noreply.github.com>
2025-10-17 23:46:29 +0530

    Create Cover in Water.cpp

---
+++ Cover in Water.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        string s;
+        cin >> n >> s; 
+        bool continuous_three_empty_cell = false;
+        int total_count_of_empty_cells = 0;
+ 
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '.' && i+1 < n && s[i + 1] == '.' && i+2 < n && s[i + 2] == '.') {
+                continuous_three_empty_cell = true;
+                break;
+            }
+            if(s[i] == '.') {
+                total_count_of_empty_cells++;
+            }
+        }
+ 
+        if(continuous_three_empty_cell) 
+            cout << 2 << endl;
+        else 
+            cout << total_count_of_empty_cells << endl;
+    }
+ 
+    return 0;
+}
